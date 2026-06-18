46044f52566d9217b3ccf373797ec42a5634084a
Sarah <sarahleoncio8@gmail.com>
2026-06-17 21:54:18 -0300

    atualizaçao

---
+++ atividade5/formulario.js
@@ -0,0 +1,8 @@
+function imprimirNome() {
+    window.alert("Usuario cadastrado")
+    let nome = document.getElementById("nome").value;
+
+    console.log("Seu nome é: ", nome)
+
+    alert("Aluno cadastrado!\n\nNome do aluno" + nome)
+}
