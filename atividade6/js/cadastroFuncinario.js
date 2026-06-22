33f1056fd80d08d1d95bafa461a88e87fd305853
Sarah Leôncio <sarahleoncio8@gmail.com>
2026-06-22 04:01:51 +0000

    códigos organizados como o do professor

---
+++ atividade6/js/cadastroFuncinario.js
@@ -0,0 +1,16 @@
+function cadastrarFuncionario() {
+    let nome = document.getElementById("nome-funcionario").value;
+    let horas = document.getElementById("horas-funcionario").value;
+    let cargo = document.getElementById("cargo-funcionario").value;
+
+    console.log(nome)
+    console.log(horas)
+    console.log(cargo)
+
+    /*
+        Registre os funcionários cadastrados e mostre cada funcionário cadastrado na
+        sua página HTML
+
+        Pode inseri-los dentro de uma sessão, tabela, paragrafo... A sua escolha.
+    */
+}
---
+++ atividade6/pages/formulario.js
@@ -0,0 +1,28 @@
+function imprimirNome() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let departamento = document.getElementById("departamento").value;
+    let temAcessoRestrito = document.getElementById("temAcessoRestrito").checked; // Retorna true ou false
+
+    console.log("====== DADOS DO CRACHÁ VIRTUAL ======");
+    console.log("Nome (String):", nome);
+    console.log("Idade (Number):", idade);
+    console.log("Departamento (String):", departamento);
+    console.log("Acesso Restrito (Boolean):", temAcessoRestrito);
+    console.log("=====================================");
+
+    window.alert(
+        "Cadastro finalizado com sucesso!\n\n" +
+        "DADOS DO CRACHÁ:\n" +
+        "Nome: " + nome + "\n" +
+        "Idade: " + idade + "\n" +
+        "Departamento: " + departamento + "\n" +
+        "Acesso Restrito: " + temAcessoRestrito
+    );
+
+    if (temAcessoRestrito) { /// se (verdadeiro)
+        window.location.href = "pages/gerente.html"
+    } else {
+        window.location.href = "pages/funcionario.html"
+    }
+}
