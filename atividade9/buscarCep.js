cc7c7332eb9ce42763b0ade401b26c8977cfe3ed
Sarah Leôncio <sarahleoncio8@gmail.com>
2026-06-30 05:40:45 +0000

    Adicionada validação de CEP

--- atividade9/buscarCep.js
+++ atividade9/buscarCep.js
@@ -1,10 +1,23 @@
 function buscarCep() {
-    let cepDigitado = document.getElementById("cep").value;
+    let cepDigitado = document.getElementById("Cep").value;
 
+    //Verificando se o usuario digitou o cep.
     if (cepDigitado === "") {
         return;
     }
-    fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
+
+    //aqui eu criei uma nova variavel pra tirar o hifen.
+    let cepLimpo = validarCep(cepDigitado)
+
+    //colocando pop-up caso usuario tenha feito errado.
+    if (cepLimpo === false) {
+        alert("CEP inválido. Digite apenas 8 caracteres.");
+        return;
+    }
+
+    //fazendo conexão com api
+    document.getElementById("Cep").value = cepLimpo;
+    fetch(`https://viacep.com.br/ws/${cepLimpo}/json/`)
     .then(function (resposta) {
         return resposta.json();
     })
@@ -20,5 +33,23 @@ function buscarCep() {
     })
 }
 
-const campoCep = document.getElementById("cep");
+//aqui continuei resolvendo o hifen com replace que tem no javacript
+function validarCep(cepComHifen){
+
+    let cep = cepComHifen.replace("-", "");
+
+    //*pra poder ter so 5 numeros vi que tem a funçao length 
+    // e usei retur pra o computador entender se deve continuar ou nao
+    if (cep.length !== 8) {
+        return false;
+    }
+
+    //esse bonus foi pq vi que tem funçao pra so poder escrever numero :o
+    if (isNaN(cep)) {
+        return false;
+    }
+    return cep;
+}
+
+const campoCep = document.getElementById("Cep");
 campoCep.addEventListener('focusout', buscarCep);
